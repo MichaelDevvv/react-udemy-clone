706c38d6491fac25c8f7b05e9701795801e1a92f
Eryk Latasiewicz <eryk.ne05@gmail.com>
2022-09-16 15:50:45 +0200

    update

--- udemy/src/components/Navbar.js
+++ udemy/src/components/Navbar.js
@@ -3,15 +3,18 @@ import Button from './Button'
 import SearchBar from './SearchBar';
 import Logo from '../images/logo-udemy.svg'
 import { BsGlobe2 } from "react-icons/bs";
+import { GrCart } from "react-icons/gr";
 
 const Navbar = () => {
   return (
     <div className='navbar'>
         <img src={Logo} alt="Logo" className='w-[93px] h-[34px] pr-[8px]' />
-        <div className='btn-tl'><span className='btn-tl--text'>Kategorie</span></div>
+
+        <div className='h-auto flex items-center justify-center mx-[12px]'><span className='text-ButtonBg font-light font-xs'>Kategorie</span></div>
         <SearchBar />
-        <div className='btn-tl'><span className='btn-tl--text'>Udemy Business</span></div>
-        <div className='btn-tl'><span className='btn-tl--text'>Nauczaj na Udemy</span></div>
+        <div className='h-auto flex items-center justify-center mx-[12px]'><span className='text-ButtonBg font-light font-xs'>Udemy Bussiness</span></div>
+        <div className='h-auto flex items-center justify-center mr-[12px]'><span className='text-ButtonBg font-light font-xs'>Nauczaj na Udemy</span></div>
+        <GrCart size={25} className='p-0 m-0' />
         <Button outline={true} className="mr-[0.8rem]">Zaloguj się</Button>
         <Button outline={false}>Zarejestruj się</Button>
         <Button outline={true}><BsGlobe2 /></Button>
--- udemy/src/components/SearchBar.js
+++ udemy/src/components/SearchBar.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { BiSearchAlt2 } from "react-icons/bi";
+
 
 const SearchBar = () => {
+
+  const [searchPhrase, setSearchPhrase] = useState('');
+
   return (
-    <div className='grow h-[46px] rounded-full border-[1px] border-buttonBg max-w-screen-md flex items-center justify-start'>
-      <input type='text' placeholder="Szukaj dowolnych elementów" className="w-full outline-0"></input>
+    <div className='w-[100%] h-[46px] rounded-full border-[1px] border-buttonBg max-w-[960px] flex items-center justify-center overflow-hidden bg-slate-50'>
+      <form action="#" className='w-full h-full flex items-center justify-center'>
+        <BiSearchAlt2 className={`mx-4 ${searchPhrase !== '' ? 'text-buttonBg' : 'text-gray-400'}`} size={25}/>
+        <input 
+          type="text" 
+          name="search" 
+          className='w-full h-full outline-none bg-transparent font-light text-sm text-black placeholder:text-gray-700' 
+          placeholder='Szukaj dowolnych elementów'
+          onChange={(e) => setSearchPhrase(e.target.value)}
+          value={searchPhrase}
+        />
+      </form>
     </div>
   )
 }
