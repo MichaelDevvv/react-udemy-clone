8b7e214a4ec0c491e56c736e8f14c98acfbe678c
Eryk Latasiewicz <eryk.ne05@gmail.com>
2022-09-24 17:28:28 +0200

    update data.js

---
+++ udemy/src/Data.js
@@ -0,0 +1,76 @@
+export const category = [
+    {
+        'id': 1,
+        'img': '../src/images/design-cat-logo.jpeg',
+        'title': 'Design',
+    },
+
+    {
+        'id': 2,
+        'img': '../src/images/dev-cat-logo.jpeg',
+        'title': 'Development',
+    },
+
+    {
+        'id': 3,
+        'img': '../src/images/markt-cat-logo.jpeg',
+        'title': 'Marketing',
+    },
+
+    {
+        'id': 4,
+        'img': '../src/images/it-cat-logo.jpeg',
+        'title': 'IT and Software',
+    },
+
+    {
+        'id': 5,
+        'img': '../src/images/pd-cat-logo.jpeg',
+        'title': 'Personal Development',
+    },
+
+    {
+        'id': 6,
+        'img': '../src/images/bs-cat-logo.jpeg',
+        'title': 'Business',
+    },
+
+    {
+        'id': 7,
+        'img': '../src/images/photo-cat-logo.jpeg',
+        'title': 'Photography',
+    },
+
+    {
+        'id': 8,
+        'img': '../src/images/music-cat-logo.jpeg',
+        'title': 'Music',
+    },
+]
+
+export const partners = [
+    {
+        'id': 1,
+        'img': '../src/images/nasdaqlogo.svg',
+    },
+    {
+        'id': 2,
+        'img': '../src/images/vglogo.svg',
+    },
+    {
+        'id': 3,
+        'img': '../src/images/boxlogo.svg',
+    },
+    {
+        'id': 4,
+        'img': '../src/images/netapplogo.svg',
+    },
+    {
+        'id': 5,
+        'img': '../src/images/evbritelogo.svg',
+    },
+    {
+        'id': 6,
+        'img': '../src/images/tcslogo.svg',
+    },
+]
