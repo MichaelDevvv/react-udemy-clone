bc3b8fe32b95219966915e7571f17560ede6b450
Michal <78083264+TenMichal@users.noreply.github.com>
2022-09-25 19:40:49 +0200

    person section

--- udemy/src/App.js
+++ udemy/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Categories from './components/Categories'
 import Navbar from './components/Navbar'
 import Partners from './components/Partners'
+import Person from './components/Person'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Categories />
       <Partners />
+      <Person />
     </div>
   )
 }
--- udemy/src/components/Button.js
+++ udemy/src/components/Button.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({outline, children, text}) => {
   return (
-    <button className={`h-[40px] ${text ? "min-w-[60px]" : null} px-3 text-sm font-bold border-[1px] border-buttonBg ml-[0.8rem] ${outline ? '' : 'bg-buttonBg text-white'}`}>
+    <button className={`h-[40px] ${text ? "min-w-[60px]" : null} px-3 text-sm font-bold border-[1px] border-buttonBg mr-[0.8rem] ${outline ? '' : 'bg-buttonBg text-white'}`}>
       {children}
     </button>
   )
